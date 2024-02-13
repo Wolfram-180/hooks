3c10807ad82709e2d6dbadef5895b6e7ea0fb1e1
Wolfram-180 <sergei.ulvis@gmail.com>
2024-02-13 22:22:07 +0300

    useReducer-step-7

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,18 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
 /* 3
 extension CompactMap<T> on Iterable<T?> {
   Iterable<T> compactMap<E>([
@@ -17,18 +29,6 @@ extension CompactMap<T> on Iterable<T?> {
           .cast();
 }
 */
-
-void main() {
-  runApp(
-    MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
-    ),
-  );
-}
 
 /* 3
 const url = 'http://bit.ly/3wh1u1S';
@@ -51,9 +51,74 @@ extension Normalize on num {
 }
 */
 
-// /* 6
+/* 6
+const url = 'http://bit.ly/3wh1u1S';
+*/
+
+// /* 7
 const url = 'http://bit.ly/3wh1u1S';
 // */
+
+// /* 7
+enum Action {
+  rotateLeft,
+  rotateRight,
+  moreVisible,
+  lessVisible,
+}
+
+@immutable
+class State {
+  final double rotationDeg;
+  final double alpha;
+
+  const State({
+    required this.rotationDeg,
+    required this.alpha,
+  });
+
+  const State.zero()
+      : rotationDeg = 0.0,
+        alpha = 1.0;
+
+  State rotateLeft() => State(
+        alpha: alpha,
+        rotationDeg: rotationDeg - 10.0,
+      );
+
+  State rotateRight() => State(
+        alpha: alpha,
+        rotationDeg: rotationDeg + 10.0,
+      );
+
+  State increaseAlpha() => State(
+        alpha: min(alpha + 0.1, 1.0),
+        rotationDeg: rotationDeg,
+      );
+
+  State decreaseAlpha() => State(
+        alpha: max(alpha - 0.1, 0.0),
+        rotationDeg: rotationDeg,
+      );
+}
+
+State reducer(State oldState, Action? action) {
+  switch (action) {
+    case Action.rotateLeft:
+      return oldState.rotateLeft();
+    case Action.rotateRight:
+      return oldState.rotateRight();
+    case Action.moreVisible:
+      return oldState.increaseAlpha();
+    case Action.lessVisible:
+      return oldState.decreaseAlpha();
+    case null:
+      return oldState;
+    default:
+      return oldState;
+  }
+}
+// 7 */
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
@@ -139,14 +204,18 @@ class HomePage extends HookWidget {
     );
 */
 
-// /* 6
-
+/* 6
     late final StreamController<double> controller;
     controller = useStreamController<double>(
       onListen: () {
         controller.sink.add(0.0);
       },
     );
+*/
+
+// /* 7
+    final store = useReducer<State, Action?>(reducer,
+        initialState: const State.zero(), initialAction: null);
 
 // */
 
@@ -214,7 +283,7 @@ class HomePage extends HookWidget {
         ],
       ),
 */
-// /* 6
+/* 6
       body: StreamBuilder<double>(
         stream: controller.stream,
         builder: (context, snapshot) {
@@ -236,10 +305,117 @@ class HomePage extends HookWidget {
           }
         },
       ),
+*/
+
+// /* 7
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RotateLeftButton(store: store),
+              RotateRightButton(store: store),
+              DecreaseAlphaButton(store: store),
+              IncreaseAlphaButton(store: store),
+            ],
+          ),
+          const SizedBox(height: 100),
+          Opacity(
+            opacity: store.state.alpha,
+            child: RotationTransition(
+              turns: AlwaysStoppedAnimation(
+                store.state.rotationDeg / 360.0,
+              ),
+              child: SizedBox(
+                height: 200,
+                child: Image.network(url),
+              ),
+            ),
+          ),
+        ],
+      ),
 // */
     );
   }
 }
+
+// /* 7
+class IncreaseAlphaButton extends StatelessWidget {
+  const IncreaseAlphaButton({
+    super.key,
+    required this.store,
+  });
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.moreVisible);
+      },
+      child: const Text('+ alpha'),
+    );
+  }
+}
+
+class DecreaseAlphaButton extends StatelessWidget {
+  const DecreaseAlphaButton({
+    super.key,
+    required this.store,
+  });
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.lessVisible);
+      },
+      child: const Text('- alpha'),
+    );
+  }
+}
+
+class RotateRightButton extends StatelessWidget {
+  const RotateRightButton({
+    super.key,
+    required this.store,
+  });
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.rotateRight);
+      },
+      child: const Text('Rotate right'),
+    );
+  }
+}
+
+class RotateLeftButton extends StatelessWidget {
+  const RotateLeftButton({
+    super.key,
+    required this.store,
+  });
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.rotateLeft);
+      },
+      child: const Text('Rotate left'),
+    );
+  }
+}
+// */
 
 /* 1
  Stream<String> getTime() => Stream.periodic(
